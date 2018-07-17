66e14af23afa88508b4bfbb7c4c968e9cf373bfe
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-07-17 12:04:08 +0800

    updated App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             movies: []
         }
